adad12da3ab9ab347f0deb97f6be2d57d893cd6f
Matthew Nguyen <114550241+R0T1N00M@users.noreply.github.com>
2022-09-27 21:09:14 -0700

    Add files via upload

---
+++ Bandit1-2skip.py
@@ -0,0 +1,32 @@
+"ssh bandit.labs.overthewire.org -p 2220 -l bandit*"
+
+import paramiko
+
+"Varables"
+host = 'bandit.labs.overthewire.org'
+port = 2220
+username = 'bandit1'
+
+"Gets passwords from levels already done before hand"
+def get_pass(n):
+    with open("Passwords") as f:
+       data = f.read().split()
+    return data[int(n)]
+
+
+password = get_pass(username[-1])
+
+"Logins you into the ssh severs"
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(host, port, username, password)
+
+"The commends given"
+cmd = 'ls -la\n'
+cmd += 'cat ./-'
+
+"Prints each commends"
+stdin,stdout,stderr = ssh.exec_command(cmd)
+outlines = stdout.readlines()
+resp = ''.join(outlines)
+print(resp)

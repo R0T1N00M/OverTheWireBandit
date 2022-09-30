ddd0976bdf3b238a02b64952566aa31249ff01d3
Matthew Nguyen <114550241+R0T1N00M@users.noreply.github.com>
2022-09-29 19:26:25 -0700

    Update Bandit2-3skip.py

--- Bandit2-3skip.py
+++ Bandit2-3skip.py
@@ -2,10 +2,12 @@
 
 import paramiko
 
+"Varables"
 host = 'bandit.labs.overthewire.org'
 port = 2220
 username = 'bandit2'
 
+"Gets passwords from levels already done before hand"
 def get_pass(n):
     with open("Passwords") as f:
        data = f.read().split()
@@ -14,13 +16,16 @@ def get_pass(n):
 
 password = get_pass(username[-1])
 
+"Logins you into the ssh severs"
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(host, port, username, password)
 
+"The commends given"
 cmd = 'ls -la\n'
 cmd += 'cat "spaces in this filename"'
 
+"Prints each commends done"
 stdin,stdout,stderr = ssh.exec_command(cmd)
 outlines = stdout.readlines()
 resp = ''.join(outlines)
